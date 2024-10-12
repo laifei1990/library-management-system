e71b8047d6aa5326be230482dd448a1faf927adf
fei.lai <fei.lai@ishansong.com>
2024-10-12 19:12:42 +0800

    1

---
+++ src/main/java/com/library/management/Main.java
@@ -0,0 +1,11 @@
+package com.library.management;
+
+/**
+ * @author laifei
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
